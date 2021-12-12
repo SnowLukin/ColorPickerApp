6d6397c45233be6ee779f787eb9f485a8018e81d
Snow Lukin <everyoursden@gmail.com>
2021-12-12 13:35:35 +0300

    Added title. Improved code.

--- ColorPickerApp/ViewController.swift
+++ ColorPickerApp/ViewController.swift
@@ -26,62 +26,71 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        pickedColorView.layer.cornerRadius = 20
+        setSlidersColor()
+        setButton()
+        
+        setStarterSettings(red: redColorSlider.value,
+                           green: greenColorSlider.value,
+                           blue: blueColorSlider.value)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        
+        let value = roundf(sender.value * 100) / 100
+        
+        switch sender {
+        case redColorSlider:
+            redValue.text = String(value)
+        case greenColorSlider:
+            greenValue.text = String(value)
+        default:
+            blueValue.text = String(value)
+        }
+        
+        updateColorView(red: redColorSlider.value,
+                        green: greenColorSlider.value,
+                        blue: blueColorSlider.value)
+    }
+    
+    @IBAction func resetToStarterSetting() {
+        setStarterSettings(red: 0.05, green: 0.27, blue: 0.49)
+    }
+    
+    // MARK: - Pravite Methods
+    private func setSlidersColor() {
         redColorSlider.minimumTrackTintColor = .red
         greenColorSlider.minimumTrackTintColor = .green
         blueColorSlider.minimumTrackTintColor = .systemBlue
-        
-        pickedColorView.layer.cornerRadius = 20
-        
+    }
+    
+    private func updateColorView(red: Float, green: Float, blue: Float) {
+        pickedColorView.backgroundColor = UIColor(red: CGFloat(red),
+                                                  green: CGFloat(green),
+                                                  blue: CGFloat(blue),
+                                                  alpha: 1)
+    }
+    
+    private func setButton() {
         resetButton.setTitle("Reset", for: .normal)
         resetButton.setTitleColor(.white, for: .normal)
         resetButton.backgroundColor = .systemRed
         resetButton.layer.cornerRadius = 10
-        
-        setStarterSettings(firstValue: 0.05, secondValue: 0.27, thirdValue: 0.49)
     }
     
-    // MARK: - IBActions
-    @IBAction func sliderMoved() {
-        setValuesToLables()
-        pickedColorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
-                                                  green: CGFloat(greenColorSlider.value),
-                                                  blue: CGFloat(blueColorSlider.value),
-                                                  alpha: 1)
+    private func setSliderSettings(slider: UISlider, label: UILabel, value: Float) {
+        slider.value = value
+        label.text = String(value)
     }
     
-    @IBAction func resetToStarterSetting() {
-        setStarterSettings(firstValue: 0.05, secondValue: 0.27, thirdValue: 0.49)
+    private func setStarterSettings(red: Float, green: Float, blue: Float) {
+        
+        setSliderSettings(slider: redColorSlider, label: redValue, value: red)
+        setSliderSettings(slider: greenColorSlider, label: greenValue, value: green)
+        setSliderSettings(slider: blueColorSlider, label: blueValue, value: blue)
+        
+        updateColorView(red: red, green: green, blue: blue)
     }
-    
-    // MARK: - Pravite Methods
-    private func setStarterSettings(firstValue: Float, secondValue: Float, thirdValue: Float) {
-        
-        redColorSlider.value = firstValue
-        redValue.text = String(redColorSlider.value)
-        
-        greenColorSlider.value = secondValue
-        greenValue.text = String(greenColorSlider.value)
-        
-        blueColorSlider.value = thirdValue
-        blueValue.text = String(blueColorSlider.value)
-        
-        pickedColorView.backgroundColor = UIColor(red: CGFloat(firstValue),
-                                                  green: CGFloat(secondValue),
-                                                  blue: CGFloat(thirdValue),
-                                                  alpha: 1)
-    }
-    
-    private func setValuesToLables() {
-        
-        var value = roundf(redColorSlider.value * 100) / 100
-        redValue.text = String(value)
-        
-        value = roundf(greenColorSlider.value * 100) / 100
-        greenValue.text = String(value)
-        
-        value = roundf(blueColorSlider.value * 100) / 100
-        blueValue.text = String(value)
-    }
-
 }
 
